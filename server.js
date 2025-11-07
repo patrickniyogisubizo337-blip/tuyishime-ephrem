02517b6224a5b3c885296a5ca6257da796ce0f99
patrickniyogisubizo337-blip <patrickniyogisubizo337@gmail.com>
2025-11-07 10:42:37 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-const express=require("express");
-const port=3000;
-const app=express();
-app.get('/',(req,res){
-    console.log("hello world");
-    res.send("hello world");
-});
-app.listen(port,()=>{
-    console.log(`the server${port}`);
-});
+p
